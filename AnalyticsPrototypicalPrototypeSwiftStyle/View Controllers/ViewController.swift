0fc2f3704b53cfd3bb8bb4782b0444782c8574c0
Kevin E. Rafferty II <krafferty@jungroup.com>
2015-06-09 19:38:59 -0700

    - Added all of the Swift classes to be under Source Control.

---
+++ AnalyticsPrototypicalPrototypeSwiftStyle/View Controllers/ViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ViewController.swift
+//  AnalyticsPrototypicalPrototypeSwiftStyle
+//
+//  Created by Kevin Rafferty on 6/9/15.
+//  Copyright © 2015 JunGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    enum JSONError: ErrorType {
+        
+        case JSONParsingError
+        
+        case JSONArrayError
+        
+        case JSONDictionaryError
+        
+    }
+    
+    // mark -
+    // mark UILabel Properties
+    // mark -
+    
+    @IBOutlet weak var averageeCPMLabel: UILabel!
+    
+    // mark -
+    // mark View Life Cycle
+    // mark -
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+    }
+    
+    // mark -
+    // mark IBAction Methods
+    // mark -
+    
+    @IBAction func onFetchInformationButtonPressed(sender: AnyObject) {
+        
+        JUNWebServices().GETAdCompletedInformation {(data: NSData) -> Void in
+            
+            do {
+                
+                let JSON = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+                
+                guard let JSONArray: NSArray = JSON["results"] as? NSArray else {
+                    
+                    throw JSONError.JSONArrayError
+                    
+                }
+                
+                var sum: NSInteger = 0
+                var averageeCPM: NSInteger = 0
+                
+                for var index = 0; index < JSONArray.count; ++index {
+                    
+                    guard let dictionary: NSDictionary = JSONArray[index] as? NSDictionary else {
+                        
+                        throw JSONError.JSONDictionaryError
+                        
+                    }
+                    
+                    guard let number: NSNumber = dictionary["ad_provider_eCPM"] as? NSNumber else {
+                        
+                        throw JSONError.JSONDictionaryError
+                        
+                    }
+                    
+                    sum += number.integerValue
+                }
+                
+                averageeCPM = sum / JSONArray.count
+                
+                // Update the eCPM UILabel
+                self.averageeCPMLabel.text = "\(averageeCPM)"
+                
+            } catch {
+                
+                print("An error has occurred parsing the JSON NSData.");
+                
+            }
+        }
+    }
+    
+    // mark -
+    // mark Helper Methods
+    // mark -
+
+//    func parseJSONData(data: NSData) -> NSDictionary {
+//        
+//        
+//    }
+    
+    func greet(name: String, day: String) -> String {
+        return "Hello \(name), today is \(day)."
+    }
+}
---
+++ AnalyticsPrototypicalPrototypeSwiftStyle/Web Service API/JUNWebServices.swift
@@ -0,0 +1,62 @@
+//
+//  JUNWebServices.swift
+//  AnalyticsPrototypicalPrototype
+//
+//  Created by Kevin Rafferty on 6/8/15.
+//  Copyright (c) 2015 JunGroup. All rights reserved.
+//
+
+import Foundation
+
+
+class JUNWebServices: NSObject {
+    
+    // mark -
+    // mark Constants
+    // mark -
+    
+    let kGETAdCompletedInformation = "https://api.keen.io/3.0/projects/550b015646f9a745baaab265/queries/extraction?api_key=f6ac29330c9dbee3fb1b249a5567236917317e5fab809eb871b35c6413a445dfad8476e9d7462b69f9d31e7b96d8d16a9311fd26d7203825d2f03126e5bb16a9f47666a21e8cfcf5c546a3757c019b8375857eda35ce3fd972a3cebd8b7c6f155985db076c4cdd6fd0412dc2e2ccaa15&event_collection=ad_completed";
+    
+    // mark -
+    // mark Web Service Methods
+    // mark -
+    
+    /**---------------------------------------------------------------------------------------
+    * @name JUN Web Service Methods
+    * ---------------------------------------------------------------------------------------
+    */
+    
+    /** This method will get the Ad Completed Event Collection information such as Ad Provider eCPM, Ad Provider Name, Virtual Currency Reward, etc.
+    *
+    *  A callback handler will provide the serialized JSON data returned from Keen in an NSDictionary format
+    */
+    func GETAdCompletedInformation(requestCompletionHandler: (data: NSData) -> Void) {
+        
+        // Create the NSURL constant from the URL path String
+        let URLPath: String = String(format: "%@&timeframe=previous_%i_hours", kGETAdCompletedInformation, 3);
+        
+        let URL: NSURL = NSURL(string: URLPath)!;
+        
+        // Create the NSURLSession constant
+        let session: NSURLSession = NSURLSession.sharedSession();
+        
+        // Create the NSURLSessionDataTask constant
+        let task: NSURLSessionDataTask = session.dataTaskWithURL(URL, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            print("Task completed")
+            
+            // If there is an error we print it out
+            if error != nil {
+                
+                print("An error occurred while retrieving the Ad Completed JSON - \(error!.localizedDescription)");
+                
+            }
+            
+            requestCompletionHandler(data: data!);
+        })!
+        
+        // The task constant is just an object with all these properties set
+        // In order to actually make the web request, we need to call resume.
+        task.resume();
+    }
+}
