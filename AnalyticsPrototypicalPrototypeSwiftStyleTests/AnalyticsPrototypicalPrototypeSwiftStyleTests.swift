824ca8bf4feff176c4f05f04304c6e29cc1e6b25
Kevin E. Rafferty II <krafferty@jungroup.com>
2015-06-12 14:17:33 -0700

    Restarted the computer and then re-added Carthage back. 	- All Unit Tests and UI Tests pass like they should now.

--- AnalyticsPrototypicalPrototypeSwiftStyleTests/AnalyticsPrototypicalPrototypeSwiftStyleTests.swift
+++ AnalyticsPrototypicalPrototypeSwiftStyleTests/AnalyticsPrototypicalPrototypeSwiftStyleTests.swift
@@ -31,12 +31,12 @@ class AnalyticsPrototypicalPrototypeSwiftStyleTests: XCTestCase {
         }
     }
     
-//    func testPerformanceExample() {
-//        
-//        // This is an example of a performance test case.
-//        self.measureBlock() {
-//            
-//            
-//        }
-//    }
+    func testPerformanceExample() {
+        
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            
+            
+        }
+    }
 }
